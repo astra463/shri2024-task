ef576e08dd0233a2a27906825f94d19045528f36
Sergei Shilov <ya.sergei240300@yandex.ru>
2024-07-13 13:19:59 +0300

    trying to optimize

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,15 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import { createHtmlPlugin } from 'vite-plugin-html';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  base: "/shri2024-task/",
-})
+  plugins: [
+    react(),
+    createHtmlPlugin(), // добавляем плагин для минификации HTML
+  ],
+  base: '/shri2024-task/',
+  build: {
+    minify: 'terser', // минификация JavaScript
+  },
+});
